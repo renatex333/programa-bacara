9b223ed9e4b060e7e563004dde1fa71f823ac561
renatex333 <renato9302@hotmail.com>
2020-09-30 18:02:34 -0300

    arrumando erros

--- jogo.py
+++ jogo.py
@@ -26,7 +26,7 @@ def entrada_de_apostadores():
 #Função que define a quantidade de baralhos que serão usados no jogo
 def define_baralhos():
     definindo_baralhos = True
-    while definindo_baralhos = True:
+    while definindo_baralhos == True:
         numero_de_baralhos = int(input("Com quantos baralhos deseja jogar?(1,6 ou 8) "))
         if numero_de_baralhos == 1 or numero_de_baralhos == 6 or numero_de_baralhos == 8:
             definindo_baralhos = False
